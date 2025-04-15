62861dc948fe23006047f52eabe9df1adc9188e5
phong <tpcreative.co@gmail.com>
2025-04-15 11:20:45 +0700

    Initial Commit

---
+++ AsynAwait/AsynAwaitApp.swift
@@ -0,0 +1,32 @@
+//
+//  AsynAwaitApp.swift
+//  AsynAwait
+//
+//  Created by phong on 15/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AsynAwaitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AsynAwaitTests/AsynAwaitTests.swift
@@ -0,0 +1,17 @@
+//
+//  AsynAwaitTests.swift
+//  AsynAwaitTests
+//
+//  Created by phong on 15/4/25.
+//
+
+import Testing
+@testable import AsynAwait
+
+struct AsynAwaitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
